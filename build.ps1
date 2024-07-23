0dda47d7fd2d7b529d5845c0ffdf31c02a5b1c44
datadiode <datadiode@users.noreply.github.com>
2024-07-23 15:14:13 +0200

    Update submodules / Remove targets from the build matrix which this fork cannot handle

--- build.ps1
+++ build.ps1
@@ -12,7 +12,11 @@ Import-Module Pscx
 
 $platDir = If($x64) { "\x64" } ElseIf ($arm64) { "\arm64" } Else { "" }
 $distname = If($x64) { "win64" } ElseIf($arm64) { "win-arm64" } Else { "win32" }
-If($vs2008) { $distname = "vs2008.$distname" }
+If($vs2008) {
+    $distname = "vs2008.$distname"
+} else {
+    if ($platDir -eq "") { $platDir = "\Win32" }
+}
 
 If($vs2008) {
     $vcvarsarch = If($x64) { "amd64" } Else { "x86" }
@@ -35,9 +39,9 @@ if($vs2008) {
     vcbuild libiconv_static\libiconv_static.vcproj "Release|$vcarch"
     $iconvLib = Join-Path (pwd) libiconv_static$platDir\Release
 } else {
-    Set-Location .\libiconv\MSVC14\
-    msbuild libiconv_static\libiconv_static.vcxproj /p:PlatformToolset=v142 /p:Configuration=Release
-    $iconvLib = Join-Path (pwd) libiconv_static$platDir\Release
+    Set-Location .\libiconv\MSVC16\
+    msbuild libiconv_static\libiconv_static.vcxproj /p:Configuration=Release
+    $iconvLib = Join-Path (pwd) $platDir\lib
 }
 
 # lxml expects iconv to be called libiconv, not libiconv_a
@@ -48,9 +52,13 @@ $iconvInc = Join-Path $PSScriptRoot libiconv\source\include
 Set-Location $PSScriptRoot
 
 Set-Location .\zlib
-cmd /c "nmake -f win32/Makefile.msc zlib.lib 2>&1"
+cmd /c "nmake -f win32/Makefile.msc zlib_a.lib 2>&1"
 $zlibLib = (pwd)
 $zlibInc = (pwd)
+
+# lxml expects zlib to be called zlib, not zlib_a
+Dir $zlibLib\zlib_a* | Copy-Item -Force -Destination {Join-Path $zlibLib ($_.Name -replace "zlib_a","zlib") }
+
 Set-Location ..
 
 Set-Location .\libxml2\win32
@@ -94,7 +102,7 @@ Function BundleRelease($name, $lib, $inc)
 if (Test-Path .\dist) { Remove-Item .\dist -Recurse }
 New-Item -ItemType Directory .\dist
 
-BundleRelease "iconv-1.14.$distname" (dir $iconvLib\iconv*) (dir $iconvInc\*)
-BundleRelease "libxml2-2.9.5.$distname" (dir $xmlLib\*) (Get-Item $xmlInc\libxml)
-BundleRelease "libxslt-1.1.30.$distname" (dir .\libxslt\win32\bin.msvc\*) (Get-Item .\libxslt\libxslt,.\libxslt\libexslt)
-BundleRelease "zlib-1.2.11.$distname" (Get-Item .\zlib\*.*) (Get-Item .\zlib\zconf.h,.\zlib\zlib.h)
+BundleRelease "iconv-1.17.$distname" (dir $iconvLib\libiconv*) (dir $iconvInc\*)
+BundleRelease "libxml2-2.10.4.$distname" (dir $xmlLib\*) (Get-Item $xmlInc\libxml)
+BundleRelease "libxslt-1.1.39.$distname" (dir .\libxslt\win32\bin.msvc\*) (Get-Item .\libxslt\libxslt,.\libxslt\libexslt)
+BundleRelease "zlib-1.2.12.$distname" (Get-Item .\zlib\*.*) (Get-Item .\zlib\zconf.h,.\zlib\zlib.h)
